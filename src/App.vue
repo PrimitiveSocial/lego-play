e5361c0fa1e57d2263f53fa3c2c22e3acf17bd1e
Elie Andraos <elie@andraos.org>
2021-01-11 14:10:16 +0200

    :construction: Setup inline editor

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-    <div id="app" class="text-center">
-        <img alt="Vue logo" src="./assets/logo.png">
+    <div id="app" class="bg-gray-100 h-full w-full fixed">
+        <div class="flex flex-rowå h-full">
+            <div class="w-1/2 p-4 border-r-4 h-full">
+                <editor></editor>
+            </div>
+            <div class="w-1/2 p-4">pane 2</div>
+        </div>
     </div>
 </template>
 
 <script>
+import Editor from "@/components/Editor";
 
 export default {
     name: 'App',
+    components: {
+        Editor
+    }
 }
 </script>
---
+++ src/components/Editor.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="h-full">
+        <button @click="run">Run</button>
+        <div id="editor" class="h-full"></div>
+    </div>
+</template>
+
+<script>
+import * as monaco from 'monaco-editor';
+
+export default {
+    data: () => {
+        return {
+            editor: null,
+        }
+    },
+    methods: {
+        run() {
+            window.console.log(this.editor.getValue());
+        }
+    },
+    mounted() {
+        this.editor = monaco.editor.create(document.getElementById('editor'), {
+            value: 'console.log("Hello, world")',
+            language: 'javascript',
+            renderLineHighlight: 'none',
+            scrollBeyondLastLine: false,
+            disableLayerHinting: true,
+            overviewRulerBorder: false,
+            minimap: {
+                enabled: false
+            },
+            scrollbar: {
+                verticalSliderSize: 5,
+                horizontalScrollbarSize: 5,
+                useShadows: false
+            }
+        });
+    }
+}
+</script>
